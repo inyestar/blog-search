edc534f2e42dcb95b6f564bd732654ff74593b1d
inyestar <inyestar@techtaka.com>
2023-03-19 20:21:04 +0900

    add rest error handling

---
+++ api/src/main/java/org/inyestar/blog/api/SearchBlogApiApplication.java
@@ -0,0 +1,11 @@
+package org.inyestar.blog.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchBlogApiApplication {
+    public static void main(String [] args) {
+        SpringApplication.run(SearchBlogApiApplication.class, args);
+    }
+}
---
+++ api/src/main/java/org/inyestar/blog/api/configuration/RestExceptionHandler.java
@@ -0,0 +1,13 @@
+package org.inyestar.blog.api.configuration;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+    @ExceptionHandler(value = Exception.class)
+    public final ResponseEntity<Object> handleInternalServerError(RuntimeException ex) {
+        return ResponseEntity.internalServerError().build();
+    }
+}
