37cc4d6fbbed8cb0e30a9a152b9aba1ac681b85a
inyestar <inyestar@techtaka.com>
2023-03-19 23:55:42 +0900

    move host value into application.yml

--- external/src/main/java/org/inyestar/blog/external/configuration/WebClientConfig.java
+++ external/src/main/java/org/inyestar/blog/external/configuration/WebClientConfig.java
@@ -8,18 +8,21 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
-
-    @Value("${auth.rest-api.kakao.key")
+    @Value("${auth.rest-api.kakao.host}")
+    private String kakaoHost;
+    @Value("${auth.rest-api.kakao.key}")
     private String kakaoRestApiKey;
-    @Value("${auth.rest-api.naver.client-id")
+    @Value("${auth.rest-api.naver.host}")
+    private String naverHost;
+    @Value("${auth.rest-api.naver.client-id}")
     private String naverClientId;
-    @Value("${auth.rest-api.naver.client-secret")
+    @Value("${auth.rest-api.naver.client-secret}")
     private String naverClientSecret;
 
     @Bean
     public WebClient kakaoWebClient() {
         return WebClient.builder()
-            .baseUrl("https://dapi.kakao.com")
+            .baseUrl(kakaoHost)
             .defaultHeader(HttpHeaders.AUTHORIZATION, kakaoRestApiKey)
             .build();
     }
@@ -27,7 +30,7 @@ public class WebClientConfig {
     @Bean
     public WebClient naverWebClient() {
         return WebClient.builder()
-            .baseUrl("https://openapi.naver.com")
+            .baseUrl(naverHost)
             .defaultHeader("X-Naver-Client-Id", naverClientId)
             .defaultHeader("X-Naver-Client-Secret", naverClientSecret)
             .build();
